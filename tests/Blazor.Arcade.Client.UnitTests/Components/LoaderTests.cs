478586df4a71c6a92aae45fa6eeab78eaac420bb
Pedro Silva <pedrosi@microsoft.com>
2022-12-18 13:58:49 -0800

    Added Loader component for app-styled spinner. Added loader to manage profiles page. Added unit tests.

---
+++ tests/Blazor.Arcade.Client.UnitTests/Components/LoaderTests.cs
@@ -0,0 +1,47 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using Blazor.Arcade.Client.Components;
+using Bunit;
+
+namespace Blazor.Arcade.Client.UnitTests.Components
+{
+    [TestClass]
+    public class LoaderTests
+    {
+        [TestMethod]
+        public void Render_IsVisibleTrue()
+        {
+            // arrange
+            var ctx = new b.TestContext();
+
+            // act
+            var comp = ctx.RenderComponent<Loader>(parameters => parameters
+                .Add(p => p.IsVisible, true));
+
+            // assert
+            var expectedHtml =
+@"
+<div class=""text-center mt-3"">
+  <span class=""spinner-border spinner-border-sm app-themed-spinner""
+        role=""status"" aria-hidden=""true"" />
+</div>
+";
+            comp.MarkupMatches(expectedHtml);
+        }
+
+        [TestMethod]
+        public void Render_IsVisibleFalse()
+        {
+            // arrange
+            var ctx = new b.TestContext();
+
+            // act
+            var comp = ctx.RenderComponent<Loader>(parameters => parameters
+                .Add(p => p.IsVisible, false));
+
+            // assert
+            comp.MarkupMatches(string.Empty);
+        }
+    }
+}
