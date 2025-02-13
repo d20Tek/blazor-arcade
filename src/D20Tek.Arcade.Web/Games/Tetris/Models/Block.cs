dc29ddb847a4edceb7a4c03d3a1ec05bc3645dc2
Pedro Silva <DarthPedro@gmail.com>
2025-02-13 14:18:21 -0800

    Defined basic types for Position and Block. Added block templates for I and O blocks to the BlockFactory.

---
+++ src/D20Tek.Arcade.Web/Games/Tetris/Models/Block.cs
@@ -0,0 +1,47 @@
+﻿namespace D20Tek.Arcade.Web.Games.Tetris.Models;
+
+internal class Block
+{
+    private int _rotationState;
+    private Position _offset;
+
+    protected Position[][] Tiles { get; }
+
+    protected Position StartOffset { get; }
+
+    public int Id { get; }
+
+    public Block(int id, Position[][] tiles, Position startOffset)
+    {
+        Id = id;
+        Tiles = tiles;
+        StartOffset = startOffset;
+        _offset = new(StartOffset.Row, StartOffset.Column);
+    }
+
+    public IEnumerable<Position> TilePositions()
+    {
+        foreach (var p in Tiles[_rotationState])
+        {
+            yield return new(p.Row + _offset.Row, p.Column + _offset.Column);
+        }
+    }
+
+    public void RotateClockwise() => _rotationState = (_rotationState + 1) % Tiles.Length;
+
+    public void RotateCounterClockwise()
+    {
+        if (_rotationState == 0)
+            _rotationState = Tiles.Length - 1;
+        else
+            _rotationState--;
+    }
+
+    public void Move(int rows, int columns) => _offset.Move(rows, columns);
+
+    public void Reset()
+    {
+        _rotationState = 0;
+        _offset = new(StartOffset.Row, StartOffset.Column);
+    }
+}
---
+++ src/D20Tek.Arcade.Web/Games/Tetris/Models/BlockFactory.cs
@@ -0,0 +1,34 @@
+﻿using D20Tek.Arcade.Web.Games.Snake.Model;
+
+namespace D20Tek.Arcade.Web.Games.Tetris.Models;
+
+internal static class BlockFactory
+{
+    public record Template(int Id, Position[][] Tiles, Position StartOffset);
+
+    public static Template[] _blocks =
+    [
+        // IBlock definition
+        new(
+            1,
+            [
+                [new(1, 0), new(1, 1), new(1, 2), new(1, 3)],
+                [new(0, 2), new(1, 2), new(2, 2), new(3, 2)],
+                [new(2, 0), new(2, 1), new(2, 2), new(2, 3)],
+                [new(0, 1), new(1, 1), new(2, 1), new(3, 1)]
+            ],
+            new(-1, 3)),
+        // OBlock definition
+        new(
+            4,
+            [ [new(0, 0), new(0, 1), new(1, 0), new(1, 1)] ],
+            new(0, 4)),
+    ];
+
+    public static Block Create(int blockId)
+    {
+        var template = _blocks[blockId];
+        return new(template.Id, template.Tiles, template.StartOffset);
+    }
+}
+
