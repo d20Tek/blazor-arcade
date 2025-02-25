9d3b6d105e8c790851b964d76c8a01cd1e13d6a4
Pedro Silva <DarthPedro@gmail.com>
2025-02-25 15:43:41 -0800

    Added enum extension method and changed ObstacleGenerator to use it.

---
+++ src/D20Tek.Arcade.Web/Common/EnumExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace D20Tek.Arcade.Web.Common;
+
+public static class EnumExtensions
+{
+    public static int Count<T>(this T _) where T : Enum
+    {
+        return Enum.GetValues(typeof(T)).Length;
+    }
+
+    public static int Count<T>() where T : struct, Enum
+    {
+        return Enum.GetValues<T>().Length;
+    }
+}
--- src/D20Tek.Arcade.Web/Games/TRexRunner/Models/ObstacleGenerator.cs
+++ src/D20Tek.Arcade.Web/Games/TRexRunner/Models/ObstacleGenerator.cs
@@ -1,9 +1,14 @@
-﻿using System.Diagnostics;
+﻿using D20Tek.Arcade.Web.Common;
+using System.Diagnostics;
 
 namespace D20Tek.Arcade.Web.Games.TRexRunner.Models;
 
 internal static class ObstacleGenerator
 {
+    private const int _percentFlyingHigh = 60;
+    private const int _flyingMultiplier = 3;
+    private static readonly int _numObstacleTypes = EnumExtensions.Count<Obstacle.Type>();
+
     public static Obstacle Create(GameState state)
     {
         var type = GenerateRandomType(state);
@@ -19,16 +24,16 @@ internal static class ObstacleGenerator
 
     private static Obstacle.Type GenerateRandomType(GameState state)
     {
-        var type = state.Rnd.Next(0, 4);
-        Debug.Assert(type >= 0 && type <= 3);
+        var type = state.Rnd.Next(0, _numObstacleTypes);
+        Debug.Assert(type >= 0 && type <= _numObstacleTypes - 1);
         return (Obstacle.Type)type;
     }
 
     private static int CalculateStartingTop(GameState state, Obstacle.Type type, Size typeSize) =>
         type switch
         {
-            Obstacle.Type.Pterodactyl when state.Rnd.Next(0, 100) > 60 =>
-                state.Layout.Viewport.Height - LayoutConstants.BottomMargin - (3 * typeSize.Height),
+            Obstacle.Type.Pterodactyl when state.Rnd.Next(0, 100) > _percentFlyingHigh =>
+                 state.Layout.Viewport.Height - LayoutConstants.BottomMargin - (_flyingMultiplier * typeSize.Height),
 
             _ => state.Layout.Viewport.Height - LayoutConstants.BottomMargin - typeSize.Height
         };
